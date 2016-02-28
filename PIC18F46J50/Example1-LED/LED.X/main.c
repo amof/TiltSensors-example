e4d336b7c5e8f90157ee6045c3f292cea9191b6e
Julien Delvaux <delvaux.ju@gmail.com>
2016-02-28 09:46:04 +0100

    Update with basic code and basic simulation.

--- PIC18F46J50/Example1-LED/LED.X/main.c
+++ PIC18F46J50/Example1-LED/LED.X/main.c
@@ -5,9 +5,19 @@
  * Created on 6 février 2016, 12:27
  */
 
+// CONFIG1L
+#pragma config WDTEN = OFF      // Watchdog Timer (Disabled - Controlled by SWDTEN bit)
+#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset (Enabled)
+#pragma config XINST = OFF      // Extended Instruction Set (Disabled)
 
 #include <xc.h>
 
 void main(void) {
+    
+    TRISB=0;
+    LATBbits.LATB0=1;
+    
+    while(1);
     return;
 }
